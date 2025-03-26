21da1d55188f3813930c7025f09d16118cf51bc0
Prosecco1516 <francescopasqualato2@gmail.com>
2025-03-26 18:25:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 # main.py
-import asyncio
-from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, filters
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 from bot.handlers import start_command, handle_message
 
-async def main():
+def main():
     application = ApplicationBuilder().token("8108284075:AAHs2urWXlt0sGzXG2EfCr8PB-XK903GGNc").build()
 
     application.add_handler(CommandHandler("start", start_command))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     print("🟢 Bot avviato...")
-    await application.run_polling(close_loop=False)
+    application.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
