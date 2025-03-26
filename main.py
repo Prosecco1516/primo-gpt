dda3b668ce169b12e4234464e7bd3b9af33b40cb
Prosecco1516 <francescopasqualato2@gmail.com>
2025-03-26 22:36:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import os
 if __name__ == "__main__":
     application = ApplicationBuilder().token(os.getenv("TELEGRAM_TOKEN")).build()
 
-    application.add_handler(start_command)
-    application.add_handler(handle_message)
+    from bot.handlers import start_handler, message_handler
+
+application.add_handler(start_handler)
+application.add_handler(message_handler)
+
 
     print("🟢 PrimoGPT avviato...")
     application.run_polling()  # Niente asyncio qui!
