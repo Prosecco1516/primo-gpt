77ee4a5b0c951685654ba1ce3595e4a9f4e5cc36
Prosecco1516 <francescopasqualato2@gmail.com>
2025-03-27 01:43:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # main.py
 import os
-import asyncio
 import nest_asyncio
+import asyncio
 from telegram.ext import ApplicationBuilder
 from handlers import start_handler, message_handler
 
 nest_asyncio.apply()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
 async def main():
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(start_handler)
@@ -19,4 +19,4 @@ async def main():
     await app.run_polling()
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.run(main())
