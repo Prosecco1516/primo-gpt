1db7a10f2cfe27e424748e08dacaac8fc282c49c
Prosecco1516 <francescopasqualato2@gmail.com>
2025-03-29 15:39:50 +0100

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,11 +1,11 @@
-  # handlers.py
-
-from telegram.ext import MessageHandler, filters
-
-# Importa la funzione da sheet.py
+# handlers.py
+from telegram.ext import MessageHandler, CommandHandler, filters
+from telegram import Update
+from telegram.ext import ContextTypes
 from sheet import handle_message
 
-def setup_handlers(application):
-    # Aggiungi handler per messaggi testuali (escludendo i comandi /)
-    message_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message)
-    application.add_handler(message_handler)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Ciao! Sono Primo, pronto ad aiutarti e ad allenarmi. Scrivimi pure.")
+
+start_handler = CommandHandler("start", start)
+message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message)
