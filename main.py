f148496dafa08aea1741b2c8fbe04b00f1e1ed77
Prosecco1516 <francescopasqualato2@gmail.com>
2025-03-29 16:34:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ import os
 import asyncio
 import nest_asyncio
 from telegram.ext import ApplicationBuilder
-from handlers import start_handler, message_handler
+from bot.handlers import start_handler, message_handler
 
 nest_asyncio.apply()
 
 async def main():
     BOT_TOKEN = os.getenv("BOT_TOKEN")
-    application = ApplicationBuilder().token(BOT_TOKEN).build()
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    application.add_handler(start_handler)
-    application.add_handler(message_handler)
+    app.add_handler(start_handler)
+    app.add_handler(message_handler)
 
     print("🟢 PrimoGPT attivo in modalità polling...")
-    await application.run_polling()
+    await app.run_polling()
 
 if __name__ == "__main__":
     asyncio.run(main())
-
+    
